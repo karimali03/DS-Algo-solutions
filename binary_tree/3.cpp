acc661e3b6d4e5e3d047cfcb7c7db1c1c2b16093
karim <ka996038@gmail.com>
2023-08-21 01:31:41 +0300

    auto commit

---
+++ binary_tree/3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+using namespace std;
+
+  struct TreeNode {
+     int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    bool leaf(TreeNode* node){
+        return node && !node->left && !node->right;
+    }
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(!root) return false;
+        if(leaf(root)) return sum==root->val;
+        return hasPathSum(root->left,sum-root->val)|| hasPathSum(root->right,sum-root->val);
+    }
+
+};
