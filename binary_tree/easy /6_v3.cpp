6c651374e132004619ad1d297120c38a25aa1f00
karim <ka996038@gmail.com>
2023-08-21 20:14:01 +0300

    auto commit

---
+++ binary_tree/easy /6_v3.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <cassert>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+	int val { };
+	TreeNode* left { };
+	TreeNode* right { };
+	TreeNode(int val) : val(val) { }
+};
+
+
+struct BinaryTree {
+	TreeNode* root { };
+	BinaryTree(int root_value) :
+			root(new TreeNode(root_value)) {
+	}
+
+	void add(vector<int> values, vector<char> direction) {
+		assert(values.size() == direction.size());
+		TreeNode* current = this->root;
+		// iterate on the path, create all necessary TreeNodes
+		for (int i = 0; i < (int) values.size(); ++i) {
+			if (direction[i] == 'L') {
+				if (!current->left)
+					current->left = new TreeNode(values[i]);
+				else
+					assert(current->left->val == values[i]);
+				current = current->left;
+			} else {
+				if (!current->right)
+					current->right = new TreeNode(values[i]);
+				else
+					assert(current->right->val == values[i]);
+				current = current->right;
+			}
+		}
+	}
+
+	void _print_inorder(TreeNode* current) {
+		if (!current)
+			return;
+		_print_inorder(current->left);
+		cout << current->val << " ";
+		_print_inorder(current->right);
+	}
+
+
+	void print_inorder() {
+		_print_inorder(root);
+		cout << "\n";
+	}
+
+   
+};
+
+class Solution {
+public:
+   int level=0;
+	bool perfect(TreeNode* root){
+       dfs(root,0);
+       return chk(root,level);
+    }
+    bool chk(TreeNode* root,int lv){
+        if(!root) return lv==0;
+        return chk(root->left,lv-1) && chk(root->right,lv-1);
+    }
+    void dfs(TreeNode* root,int lv=0){
+        if(!root) {
+            level=lv;
+            return;
+        }
+        dfs(root->left,lv+1);
+        dfs(root->right,lv+1);
+    }
+};
+int main() {
+	BinaryTree tree(1);
+     Solution s;
+	tree.add( { 2}, { 'L' });
+	tree.add( { 2}, { 'R' });
+
+    cout<<s.perfect(tree.root);
+   
+	return 0;
+}
